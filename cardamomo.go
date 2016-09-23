8692c845cbdbde278324766e8bc4c91e65adf124
Manuel Luis  Garrido Gutiérrez <mlgarrido@Mac-mini-de-Manuel.local>
2016-09-23 09:42:12 +0200

    Update router with goroutines

--- cardamomo.go
+++ cardamomo.go
@@ -110,7 +110,7 @@ func (c *Cardamomo) Run() {
 				}
 	      request := NewRequest(w, req, currentRoute)
 	      response := NewResponse(w, req)
-	      go currentRoute.callback(request, response)
+	      currentRoute.callback(request, response)
 	    } else {
 				if c.Config["production"]["debug"] == "true" {
 					fmt.Printf("\n HTTP ERROR: 404 - 1")
