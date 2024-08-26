ad4ebc27d7d7e21b916a63dea61ecf941c3fa1a6
Manuel Luis Garrido Gutiérrez <mluisgarrido@gmail.com>
2024-08-26 10:54:59 +0200

    Update socket client

--- socket.client.go
+++ socket.client.go
@@ -55,67 +55,52 @@ func (sc *SocketClient) GetIP() string {
 }
 
 func (sc *SocketClient) Listen() {
-  defer func() {
-    if err := recover(); err != nil {
-      fmt.Println("panic occurred:", err)
-    }
-  }()
+    defer func() {
+      if err := recover(); err != nil {
+        fmt.Println("Panic occurred on \"Listen\":", err)
+      }
+    }()
 
-  for {
-    var msg SocketClientMessage
-    err := websocket.JSON.Receive(sc.WebSocket, &msg)
-    if err == io.EOF || websocket.IsCloseError(err) {
-      // Error
-      // Disconnect and remove from client
-      for index, action := range sc.actions {
-        index = 1
-        _ = index
+    for {
+      var msg SocketClientMessage
+      err := websocket.JSON.Receive(sc.WebSocket, &msg)
+      if err != nil {
+        fmt.Printf("Socket error: %s\n", err)
 
-        if action != nil {
-          if "onDisconnect" == action.action {
+        for _, action := range sc.actions {
+          if action != nil && action.action == "onDisconnect" {
             var params map[string]interface{}
             action.callback(params)
           }
         }
+
+        sc.route.clients.Delete(sc.id)
+
+        return
       }
 
-      sc.route.clients.Delete(sc.id)
+      switch msg.Action {
+        case "CardamomoSocketInit":
+          params := make(map[string]interface{})
+          params["id"] = sc.GetID()
+          sc.Send("CardamomoSocketInit", params)
 
-      return
-    } else if err != nil {
-      // Error
-      fmt.Printf("Socket error: %s - 1", err)
-    } else {
-      // Send initial data
-      if msg.Action == "CardamomoSocketInit" {
-        params := make(map[string]interface{})
-        params["id"] = sc.GetID()
+        case "CardamomoPing":
+          sc.Send("CardamomoPong", make(map[string]interface{}))
 
-        sc.Send("CardamomoSocketInit", params)
-      } else if msg.Action == "CardamomoPing" {
-        sc.Send("CardamomoPong", make(map[string]interface{}))
-      } else {
-        // Common actions
-        for index, action := range sc.actions {
-          index = 1
-          _ = index
-
-          if action != nil {
-            if msg.Action == action.action {
+        default:
+          for _, action := range sc.actions {
+            if action != nil && msg.Action == action.action {
               var params map[string]interface{}
-              err := json.Unmarshal([]byte(msg.Params), &params)
-              if err != nil {
-                // Error
-                fmt.Printf("Socket error: %s - 2", err)
+              if err := json.Unmarshal([]byte(msg.Params), &params); err != nil {
+                fmt.Printf("Error unmarshalling params: %s\n", err)
               } else {
                 action.callback(params)
               }
             }
           }
-        }
       }
     }
-  }
 }
 
 func (sc *SocketClient) OnSocketAction(action string, callback SockActionFunc) {
@@ -124,8 +109,27 @@ func (sc *SocketClient) OnSocketAction(action string, callback SockActionFunc) {
 }
 
 func (sc *SocketClient) Send(action string, params interface{}) {
+  defer func() {
+    if err := recover(); err != nil {
+      fmt.Println("Panic occurred on \"Send\":", err)
+    }
+  }()
+
   msg := SocketMessage{Action:action, Params: params}
-  websocket.JSON.Send(sc.WebSocket, msg)
+  err := websocket.JSON.Send(sc.WebSocket, msg)
+
+  if err != nil {
+    fmt.Printf("Socket error: %s\n", err)
+
+    for _, action := range sc.actions {
+      if action != nil && action.action == "onDisconnect" {
+        var params map[string]interface{}
+        action.callback(params)
+      }
+    }
+
+    sc.route.clients.Delete(sc.id)
+  }
 }
 
 func getIP(r *http.Request) (string, error) {
